9cf29c3d0a4ee06e8e1bada22f8b6ca725cb4d99
JuanB12 <jcamilobb03@gmail.com>
2022-07-25 20:45:43 -0500

    Prueba de petición, http

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:trabajo_discapacidades/pages/consultas.dart';
 import 'package:trabajo_discapacidades/pages/home.dart';
@@ -5,6 +6,7 @@ import 'package:trabajo_discapacidades/pages/login.dart';
 import 'package:trabajo_discapacidades/pages/registro.dart';
 import 'package:trabajo_discapacidades/pages/registro_nat.dart';
 import 'package:trabajo_discapacidades/pages/registro_opc.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -16,20 +18,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Búsqueda de empleo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: const MyHomePage(),
-      routes: {
-        "/": (BuildContext context) => const MyHomePage(),
-        "/registro": (BuildContext context) => const RegistroPage(),
-        "/registro_opc":(BuildContext context) => const RegistroOpcPage(),
-        "/registro_nat":(BuildContext context) => const RegistroNatPage(),
-        "/login":(BuildContext context) => const LoginPage(),
-        "/consultas":(BuildContext context) => const ConsultasPage(),
-      }
-    );
+        title: 'Búsqueda de empleo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        //home: const MyHomePage(),
+        routes: {
+          "/": (BuildContext context) => const MyHomePage(),
+          "/registro": (BuildContext context) => const RegistroPage(),
+          "/registro_opc": (BuildContext context) => const RegistroOpcPage(),
+          "/registro_nat": (BuildContext context) => const RegistroNatPage(),
+          "/login": (BuildContext context) => const LoginPage(),
+          "/consultas": (BuildContext context) => const ConsultasPage(),
+        });
   }
 }
 
+void peticion(List<String> arguments) {
+  final url = Uri.parse('https://jsonplaceholder.typicode.com/albums/1');
+  http.get(url).then((res) {});
+  print(url);
+}
