96f2f808d5677e39879c75661d8b5ee30af8c7d4
JuanB12 <jcamilobb03@gmail.com>
2022-07-29 12:24:04 +0200

    modificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:trabajo_discapacidades/pages/registro_opc.dart';
 import 'package:trabajo_discapacidades/pages/consultas_empleos.dart';
 import 'package:trabajo_discapacidades/pages/consultas_empresas.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -21,23 +20,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'FindMe', //Nombre de la aplicación
-        theme: ThemeData( //Temas
+        theme: ThemeData(
+          //Temas
           primarySwatch: Colors.blue,
           scaffoldBackgroundColor: const Color(0xffEEF8FB),
-          elevatedButtonTheme: ElevatedButtonThemeData( //Tema de los botones
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            //Tema de los botones
             style: ElevatedButton.styleFrom(
-              primary: const Color(0xff0096C7),
-              textStyle: const TextStyle(
-                fontFamily: 'Rosario',
-                fontSize: 25,
-              ),
-              padding: const EdgeInsets.all(30),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30))
-              )
-              ), 
+                primary: const Color(0xff0096C7),
+                textStyle: const TextStyle(
+                  fontFamily: 'Rosario',
+                  fontSize: 25,
+                ),
+                padding: const EdgeInsets.all(30),
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30)))),
           ),
-          
         ),
         //home: const MyHomePage(),
 
@@ -50,9 +48,10 @@ class MyApp extends StatelessWidget {
           "/registro_emp": (BuildContext context) => const RegistroEmpPage(),
           "/login": (BuildContext context) => const LoginPage(),
           "/consultas": (BuildContext context) => const ConsultasPage(),
-          "/consultas_empleos": (BuildContext context) => const ConsultasEmpleosPage(),
-          "/consultas_empresas": (BuildContext context) => const ConsultasEmpresasPage(),
-
+          "/consultas_empleos": (BuildContext context) =>
+              const ConsultasEmpleosPage(),
+          "/consultas_empresas": (BuildContext context) =>
+              const ConsultasEmpresasPage(),
         });
   }
 }
